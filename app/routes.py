4ba1fd9ffe8e20deba40095ae4622cec2c80f1aa
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-17 11:13:31 -0700

    added responses 400 and 404 to handle invalid or nonexistent book in books/<book_id>

--- app/routes.py
+++ app/routes.py
@@ -10,14 +10,17 @@ class Book:
         self.description = description
 
 books = [
-    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+    Book(1, "Book 1", "A fantasy novel set in an imaginary world."),
+    Book(2, "Book 2", "A fantasy novel set in an imaginary world."),
+    Book(3, "Book 3", "A fantasy novel set in an imaginary world.")
 ] 
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def specific_book(book_id):
-    book_id = int(book_id)
+    try:
+        book_id = int(book_id)
+    except: 
+        return {"message": f"book {book_id} was invalid"}, 400
     for book in books:
         if book.id == book_id:
             return {
@@ -25,6 +28,7 @@ def specific_book(book_id):
                 "title": book.title,
                 "description": book.description
                     }
+    return {"message": f"book {book_id} was not found"}, 404
 
 @books_bp.route("", methods=["GET"])
 def books_list():
