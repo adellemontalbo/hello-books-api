385889541c151a18d96d6ac98ce6596fbc9973a3
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-17 11:39:58 -0700

    made helper function to validate book_id, used abort to return response object if id is invalid

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 hello_world_bp = Blueprint("hello_world", __name__)
 books_bp = Blueprint("books", __name__, url_prefix="/books")
@@ -15,20 +15,27 @@ books = [
     Book(3, "Book 3", "A fantasy novel set in an imaginary world.")
 ] 
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def specific_book(book_id):
+def validate_book(book_id):
     try:
         book_id = int(book_id)
     except: 
-        return {"message": f"book {book_id} was invalid"}, 400
+        abort(make_response({"message": f"book {book_id} was invalid"}, 400)) # using abort instead of return so it doesn't continue on with our code in specific_book, abort expects a respoonse object so we need to use make_response to return an object
     for book in books:
         if book.id == book_id:
-            return {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
-                    }
-    return {"message": f"book {book_id} was not found"}, 404
+            return book
+    abort(make_response({"message": f"book {book_id} was not found"}, 404))
+
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def specific_book(book_id):
+    book = validate_book(book_id)
+
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+            }
+
 
 @books_bp.route("", methods=["GET"])
 def books_list():
