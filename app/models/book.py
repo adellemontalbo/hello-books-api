d36357dd7d2da1a5ec373b917a70fc8c23930725
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-27 12:54:09 -0700

    completed models setup

---
+++ app/models/book.py
@@ -0,0 +1,12 @@
+from app import db
+
+'''
+from app, import db
+We define a new class and name it after our model, usually singular
+By default, SQLAlchemy will use the lowercase version of this class name as the name of the table it will create.
+In model definitions, if we don't like the default name that SQLAlchemy picks we can specify a different name for the table, using the __tablename__ property. There is more to research here.
+'''
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
--- app/routes.py
+++ app/routes.py
@@ -1,64 +1,49 @@
 from flask import Blueprint, jsonify, abort, make_response
 
-hello_world_bp = Blueprint("hello_world", __name__)
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
 
-books = [
-    Book(1, "Book 1", "A fantasy novel set in an imaginary world."),
-    Book(2, "Book 2", "A fantasy novel set in an imaginary world."),
-    Book(3, "Book 3", "A fantasy novel set in an imaginary world.")
-] 
+# books = [
+#     Book(1, "Book 1", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Book 2", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Book 3", "A fantasy novel set in an imaginary world.")
+# ] 
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except: 
-        abort(make_response({"message": f"book {book_id} was invalid"}, 400)) # using abort instead of return so it doesn't continue on with our code in specific_book, abort expects a respoonse object so we need to use make_response to return an object
-    for book in books:
-        if book.id == book_id:
-            return book
-    abort(make_response({"message": f"book {book_id} was not found"}, 404))
-
-
-@books_bp.route("/<book_id>", methods=["GET"])
-def specific_book(book_id):
-    book = validate_book(book_id)
-
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-            }
+# def validate_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except: 
+#         abort(make_response({"message": f"book {book_id} was invalid"}, 400)) # using abort instead of return so it doesn't continue on with our code in specific_book, abort expects a respoonse object so we need to use make_response to return an object
+#     for book in books:
+#         if book.id == book_id:
+#             return book
+#     abort(make_response({"message": f"book {book_id} was not found"}, 404))
 
 
-@books_bp.route("", methods=["GET"])
-def books_list():
-    books_list = []
-    for book in books:
-        books_list.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-    return jsonify(books_list), 200 # makes a Response object, used when returning a list
+# @books_bp.route("/<book_id>", methods=["GET"])
+# def specific_book(book_id):
+#     book = validate_book(book_id)
+
+#     return {
+#         "id": book.id,
+#         "title": book.title,
+#         "description": book.description
+#             }
 
 
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def hello_JSON():
-    return {
-        "name": "JSON SURE",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"],
-    }, 200
+# @books_bp.route("", methods=["GET"])
+# def books_list():
+#     books_list = []
+#     for book in books:
+#         books_list.append({
+#             "id": book.id,
+#             "title": book.title,
+#             "description": book.description
+#         })
+#     return jsonify(books_list), 200 # makes a Response object, used when returning a list
 
-
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def hello_world():
-    my_response_body = "Hello, World!"
-    return my_response_body, 201
