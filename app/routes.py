d0f9aee367e96b3c972acae1ef57780917febf68
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-27 14:13:44 -0700

    modified handle_books function to handle GET requests

--- app/routes.py
+++ app/routes.py
@@ -13,14 +13,26 @@ For a little more flexibility, we could choose to use "/" as the route path and 
 the following function will execute whenever a matching HTTP request is received
 We use the request object to get information about the HTTP request. We want to get the request's JSON body, so we use request.get_json(). This method "Pythonifies" the JSON HTTP request body by converting it to a Python dictionary.
 '''
-@books_bp.route("", methods=["POST"])
+@books_bp.route("", methods=["GET", "POST"])
 def handle_books():
-    request_body = request.get_json()
-    #create an instance of Book
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
-    #the databases way of collecting changes that need to be made, here we are saying we want the database to add a new book and them commit the collected changes
-    db.session.add(new_book)
-    db.session.commit()
-    # we want to return a response object from Flask endpoint functions
-    return make_response(f"Book {new_book.title} successfully created", 201)
+    if request.method == "GET":
+        books = Book.query.all() # returns a list of book instances
+        books_response = [{
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            } for book in books]
+        return jsonify(books_response)
+
+    elif request.method == "POST":
+        request_body = request.get_json()
+        #create an instance of Book
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
+        #the databases way of collecting changes that need to be made, here we are saying we want the database to add a new book and them commit the collected changes
+        db.session.add(new_book)
+        db.session.commit()
+        # we want to return a response object from Flask endpoint functions
+        return make_response(f"Book {new_book.title} successfully created", 201)
+
+
