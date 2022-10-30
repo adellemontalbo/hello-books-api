190deefc7c5d79e8b13ff43707a7b098e830136a
Adelle Montalbo <adellemontalbo@gmail.com>
2022-10-30 16:47:01 -0700

    created helper function to validate book, created endpoint to get one book by book_id

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import db
 from app.models.book import Book
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort
 
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
@@ -13,11 +13,31 @@ For a little more flexibility, we could choose to use "/" as the route path and 
 the following function will execute whenever a matching HTTP request is received
 We use the request object to get information about the HTTP request. We want to get the request's JSON body, so we use request.get_json(). This method "Pythonifies" the JSON HTTP request body by converting it to a Python dictionary.
 '''
+#helper functions
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({f"message": "Book {book_id} is invalid"}, 400))
+
+    book = Book.query.get(book_id)
+   
+    if not book:
+        abort(make_response({f"message": "Book {book_id} does not exist"}, 404))
+    return book 
+
+
+
 @books_bp.route("", methods=["GET"]) #Here we are defining a route. This decorator (a decorator is a function that modifies another function) tells Python when to call this function
 def get_all_books():
     books = Book.query.all() # returns a list of book instances
     books_response = [book.to_dict() for book in books]
     return jsonify(books_response), 200
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def get_one_book(book_id):
+    book = validate_book(book_id)
+    return book.to_dict()
 
 @books_bp.route("", methods=["POST"])
 def post_book():
